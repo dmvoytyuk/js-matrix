4ca508025f9e3ec75292a0b4b2ea0db13ac3b047
dmvoytyuk <dmvoytyuk@gmail.com>
2024-02-12 21:08:01 +0100

    Update matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -1,11 +1,43 @@
 const canvasEl = document.querySelector(".matrix");
-const dpr = window.devicePixelRatio;
+// const dpr = window.devicePixelRatio;
 canvasEl.height = window.innerHeight;
 canvasEl.width = window.innerWidth;
 const ctx = canvasEl.getContext("2d");
+ctx.font = "20px Tahoma";
 
-ctx.fillStyle = "rgba(0,0,0,1)";
-ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+const colsNumbers = canvasEl.width / 15;
+console.log(Math.round(colsNumbers));
+const posY = [];
+
+for (let i = 0; i < colsNumbers; i++) {
+  posY[i] = -Math.round(Math.random() * 1000);
+  // console.log(posY[i]);
+}
+
+function matrix() {
+  // draw background
+  ctx.fillStyle = "rgba(0,0,0,0.09)";
+  ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+  // draw raws
+  let x = 3;
+  for (let i = 0; i < colsNumbers; i++) {
+    ctx.fillStyle = "rgba(0,255,50,1)";
+    let char = Math.random() > 0.4 ? 1 : 0;
+    ctx.fillText(char, x, posY[i]);
+    x = x + 20;
+  }
+  for (let i = 0; i < colsNumbers; i++) {
+    posY[i] = posY[i] + 20;
+    if (posY[i] > canvasEl.height + Math.round(Math.random() * 100)) {
+      posY[i] = -Math.round(Math.random() * 1000);
+    }
+  }
+
+  setTimeout(matrix, 100);
+}
+
+matrix();
+
 window.addEventListener("resize", changeCanvasSize);
 function changeCanvasSize() {
   canvasEl.height = window.innerHeight;
@@ -15,31 +47,5 @@ function changeCanvasSize() {
   location.reload();
 }
 
-ctx.font = "20px Tahoma";
-let y = -9;
-function matrix() {
-  ctx.fillStyle = "rgba(0,0,0,0.098)";
-  ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
-  // draw raws
-  let x = 10;
-  //   let y = Math.random() * 1000;
-  for (let i = 0; i < canvasEl.width / 15; i++) {
-    ctx.fillStyle = "rgba(0,255,50,1)";
-    let char = Math.random() > 0.4 ? 1 : 0;
-    ctx.fillText(char, x, y);
-    x = x + 20;
-  }
-
-  //draw columns
-  ctx.fillStyle = "rgba(0,255,50,1)";
-  let char = Math.random() > 0.4 ? 1 : 0;
-  ctx.fillText(char, x, y);
-  setTimeout(matrix, 70);
-  y = y + 20;
-  if (y > canvasEl.height + 20) {
-    y = -9;
-    // console.log(y);
-  }
-}
-
-matrix();
+ctx.fillStyle = "rgba(0,0,0,1)";
+ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
