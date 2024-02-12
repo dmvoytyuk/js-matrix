f485f13cdecc3a92e6599ad573432312e96d0e93
dmvoytyuk <dmvoytyuk@gmail.com>
2024-02-12 20:32:29 +0100

    Update matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -1,10 +1,9 @@
 const canvasEl = document.querySelector(".matrix");
 const dpr = window.devicePixelRatio;
-canvasEl.height = window.innerHeight * dpr;
-canvasEl.width = window.innerWidth * dpr;
-console.log(dpr);
+canvasEl.height = window.innerHeight;
+canvasEl.width = window.innerWidth;
 const ctx = canvasEl.getContext("2d");
-ctx.scale(dpr, dpr);
+
 ctx.fillStyle = "rgba(0,0,0,1)";
 ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 window.addEventListener("resize", changeCanvasSize);
@@ -13,10 +12,11 @@ function changeCanvasSize() {
   canvasEl.width = window.innerWidth;
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+  location.reload();
 }
 
 ctx.font = "20px Tahoma";
-let y = 0;
+let y = -9;
 function matrix() {
   ctx.fillStyle = "rgba(0,0,0,0.05)";
   ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
@@ -34,10 +34,10 @@ function matrix() {
   ctx.fillStyle = "rgba(0,255,50,1)";
   let char = Math.random() > 0.4 ? 1 : 0;
   ctx.fillText(char, x, y);
-  setTimeout(matrix, 50);
+  setTimeout(matrix, 20);
   y = y + 20;
   if (y > canvasEl.height + 20) {
-    y = 0;
+    y = -9;
     // console.log(y);
   }
 }
